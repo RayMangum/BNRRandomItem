66a63c106eaece3f17bdbc1fb18fd6a6e273784f
Ray Mangum <raymangum@gmail.com>
2014-04-12 20:09:45 -0400

    Initial Commit

---
+++ RandomItem/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  RandomItem
+//
+//  Created by James Ray Mangum on 4/12/14.
+//  Copyright (c) 2014 Ray Mangum. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
