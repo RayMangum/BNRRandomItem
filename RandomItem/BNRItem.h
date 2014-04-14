df2e1696fb679eb427e37e335e2b5047a7b74896
Ray Mangum <raymangum@gmail.com>
2014-04-13 22:10:09 -0400

    End of chapter

---
+++ RandomItem/BNRItem.h
@@ -0,0 +1,39 @@
+//
+//  BNRItem.h
+//  RandomItem
+//
+//  Created by James Ray Mangum on 4/12/14.
+//  Copyright (c) 2014 Ray Mangum. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface BNRItem : NSObject
+{
+    NSString *_itemName;
+    NSString *_serialNumber;
+    int _valueInDollars;
+    NSDate *_dateCreated;
+}
+
++ (instancetype)randomItem;
+
+// Designated initializer
+- (instancetype)initWithItemName:(NSString *)name
+                  valueInDollars:(int)value
+                    serialNumber:(NSString *)sNumber;
+
+- (instancetype)initWithItemName:(NSString *)name;
+
+- (void) setItemName:(NSString *)str;
+- (NSString *)itemName;
+
+- (void)setSerialNumber:(NSString *)str;
+- (NSString *)serialNumber;
+
+- (void)setValueInDollars:(int)v;
+- (int)valueInDollars;
+
+- (NSDate *)dateCreated;
+
+@end
